312219a21a24b36941d2e6b26da13296fc00317c
Lars Janssen <lars.janssen@haw-hamburg.de>
2024-02-11 00:26:34 +0100

    Integrated Ubuntu and Conda RCs.

---
+++ .bashrcSources/aliases.sh
@@ -0,0 +1,9 @@
+alias l='ls --color -l'
+
+alias nf='neofetch'
+
+alias bashrc='nano /home/lars/.bashrc'
+
+alias gay='lolcat'
+
+alias setBtAudio="pactl set-default-sink $(pactl list short sinks | tail -n1 | cut -d'b' -f1)"
