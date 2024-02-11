8a736262e87c2149de944fba533be7c81dbab579
Lars Janssen <lars.janssen@haw-hamburg.de>
2024-02-11 01:58:47 +0100

    added aliases

--- bashrcsources/aliases.sh
+++ bashrcsources/aliases.sh
@@ -5,15 +5,17 @@ alias la='l -A'
 # ~/dotfiles/ related aliases
 alias reStow='cd ~/dotfiles && stow -R . && cd -'
 alias dotpush='cd ~/dotfiles && git status && git commit -a && git push && cd -'
-
-alias nf='neofetch'
+alias realias='source ~/bashrcsources/aliases.sh'
 
 # edits for .bashrc files
 alias editbashrc='nvim ~/.bashrc'
 alias editalias='nvim ~/bashrcsources/aliases.sh'
 
+# uncategorized aliases
+alias n='nvim'
+
 alias gay='lolcat'
 
-alias setBtAudio="pactl set-default-sink $(pactl list short sinks | tail -n1 | cut -d'b' -f1)"
+alias nf='neofetch'
 
-alias realias='source ~/bashrcsources/aliases.sh'
+alias setBtAudio="pactl set-default-sink $(pactl list short sinks | tail -n1 | cut -d'b' -f1)"
