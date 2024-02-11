a417f93f58165fd66cb5e8bea52be8353f6fd835
Lars Janssen <lars.janssen@haw-hamburg.de>
2024-02-11 02:18:11 +0100

    :

--- bashrcsources/aliases.sh
+++ bashrcsources/aliases.sh
@@ -3,19 +3,24 @@ alias l='ls --color -l'
 alias la='l -A'
 
 # ~/dotfiles/ related aliases
-alias reStow='cd ~/dotfiles && stow -R . && cd -'
-alias dotpush='cd ~/dotfiles && git status && git commit -a && git push && cd -'
+alias todotdir='cd ~/dotfiles'
+alias reStow='todotdir && stow -R . && cd -'
+alias dotpush='todotdir && git status && git commit -a && git push && cd -'
+alias dotstatus='todotdir && git status && cd -'
+alias dotaddall='todotdir && git add . && cd -'
 alias realias='source ~/bashrcsources/aliases.sh'
 
 # edits for .bashrc files
-alias editbashrc='nvim ~/.bashrc'
-alias editalias='nvim ~/bashrcsources/aliases.sh'
+alias editb='nvim ~/.bashrc'
+alias edita='nvim ~/bashrcsources/aliases.sh'
+
+# audio
+alias setBtAudio="pactl set-default-sink \$(pactl list short sinks | tail -n1 | cut -d'b' -f1)"
 
 # uncategorized aliases
 alias n='nvim'
+alias r='ranger'
 
 alias gay='lolcat'
 
 alias nf='neofetch'
-
-alias setBtAudio="pactl set-default-sink \$(pactl list short sinks | tail -n1 | cut -d'b' -f1)"
