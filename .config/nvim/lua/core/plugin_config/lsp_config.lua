8f8020c9402aa4553045fa31ed57eb8116d07a01
Lars <lars.e.janssen@gmail.com>
2024-02-22 01:50:00 +0100

    lsp

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,19 +1,20 @@
 local servers = { "lua_ls", "marksman", "bashls", "jdtls", "jedi_language_server", "dockerls", "docker_compose_language_service" }
 
 require("mason").setup()
-
+require'lspconfig'.lua_ls.setup{}
 require("mason-lspconfig").setup({
   ensure_installed = servers,
 })
 
-local on_attach = function(_, bufnr)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.renamem, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+local on_attach = function(_, _)
+  local opts = { buffer = ev.buf }
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.renamem, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
 
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
 
 local lspconfig = require('lspconfig')
