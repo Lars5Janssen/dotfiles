f4c584518359524e36563d2d67699b79a4d68e66
Lars Janssen <lars.e.janssen@gmail.com>
2024-06-03 13:14:11 +0200

    fun

--- .dotfilesextended/scripts/fun.sh
+++ .dotfilesextended/scripts/fun.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 while [[ true ]]; do
+
+    keyboardBacklightDevice='intel_backlight'
+    
+    currentBrightness="$(brightnessctl --device $keyboardBacklightDevice get)"
+    if [  $currentBrightness = '0' ]; then
+        brightnessctl --quiet --device $keyboardBacklightDevice set 100%
+    else
+        brightnessctl --quiet --device $keyboardBacklightDevice set 0
+    fi
    ./toggleKbBacklight.sh 
 done
