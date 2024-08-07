ac7076a734bd2ce223a6af083e9c169b8bbba5be
Lars Janssen <lars.e.janssen@gmail.com>
2024-08-07 14:50:49 +0200

    nvim before dap

--- .config/nvim/lua/plugins/nvim-jdtls.lua
+++ .config/nvim/lua/plugins/nvim-jdtls.lua
@@ -7,6 +7,9 @@ return {
 			local conf = {
 				cmd = { "/usr/bin/jdtls" },
 				root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
+				settings = {
+					["java.settings.url"] = "./configs/jdtls/settings.perf",
+				},
 			}
 			require("jdtls").start_or_attach(conf)
 		end,
