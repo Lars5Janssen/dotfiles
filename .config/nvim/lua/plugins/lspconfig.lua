284fa53fecb714e563f8593045011aa6d6db3765
Lars Janssen <lars.e.janssen@gmail.com>
2024-11-12 18:22:32 +0100

    stuff

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,33 +1,34 @@
 return {
 
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local lspconfig = require("lspconfig")
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-            lspconfig.autotools_ls.setup({})
-            lspconfig.gopls.setup({})
-            lspconfig.bashls.setup({})
-            lspconfig.docker_compose_language_service.setup({})
-            lspconfig.dockerls.setup({})
-            lspconfig.jedi_language_server.setup({})
-            lspconfig.jsonls.setup({})
-            lspconfig.rust_analyzer.setup({})
-            lspconfig.taplo.setup({})
-            lspconfig.yamlls.setup({})
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
-                            disable = { "different-requires" },
-                        },
-                    },
-                },
-            })
-        end,
-    },
+			lspconfig.autotools_ls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.bashls.setup({})
+			lspconfig.docker_compose_language_service.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.jedi_language_server.setup({})
+			lspconfig.basedpyright.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.taplo.setup({})
+			lspconfig.yamlls.setup({})
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+							disable = { "different-requires" },
+						},
+					},
+				},
+			})
+		end,
+	},
 }
