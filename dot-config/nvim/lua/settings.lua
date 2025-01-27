0e31cf0de4976af352883e50b91f45288c8a3750
Lars Janssen <lars.e.janssen@gmail.com>
2025-01-27 11:35:22 +0100

    linewrap

--- dot-config/nvim/lua/settings.lua
+++ dot-config/nvim/lua/settings.lua
@@ -3,6 +3,7 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.smartindent = true
+vim.wo.wrap = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = false
@@ -11,14 +12,14 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
-vim.opt.foldmethod=indent
+vim.opt.foldmethod = indent
 --vim.opt.colorcolumn = "80"
 vim.opt.expandtab = true
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = "*",
-	callback = function()
-		vim.opt.formatoptions:remove({ "o" })
-	end,
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions:remove({ "o" })
+    end,
 })
