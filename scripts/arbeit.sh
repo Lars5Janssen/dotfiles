68242ee2cbd33af087375bf1dac29060ecf8331c
Lars Janssen <lars.e.janssen@gmail.com>
2025-03-11 12:29:01 +0100

    arbeit.sh

--- scripts/arbeit.sh
+++ scripts/arbeit.sh
@@ -2,32 +2,42 @@
 # A script that activates when the computer is connected to the wifi of my workplace
 # it then executes my "morning work routine" whatever that my be
 
-ndnd
-sleep 2
+send-notification ()
+{
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$1" "$2" $3 $4 
+}
+switch-workspace ()
+{
+    hyprctl dispatch workspace $1
+}
 
-WIFISSID="$(iwctl station wlan0 show | grep "Connected network" | awk '{ print $3 }')"
+ndnd
+switch-workspace 9
+hyprctl dispatch exec signal-desktop
+hyprctl dispatch exec firefox web.whatsapp.com
+sleep 5
+switch-workspace 1
+
+# WIFISSID="$(iwctl station wlan0 show | grep "Connected network" | awk '{ print $3 }')"
+WIFISSID="$(nmcli device show | grep "GENERAL.CONNECTION" | awk '{ print $2}' | head --lines=1)"
 
 if [[ "$WIFISSID" != "Science-Hotspot" ]]; then
     echo "Not at work"
+    send-notification "Not at Work"
     exit
 fi
 
 echo "At work; Executing work routine"
+send-notification "At Work" "Executing work routine"
 
-hyprctl dispatch workspace 10
-hyprctl dispatch exec /opt/cisco/secureclient/bin/vpnui
-sleep 2
-hyprctl dispatch focuswindow title:privat.kdbx
-sleep 12
-
-hyprctl dispatch workspace 2
-hyprctl dispatch workspace 1
-hyprctl dispatch workspace 10
-hyprctl dispatch workspace 2
 sleep 2
 
-hyprctl dispatch exec "flatpak run org.mozilla.Thunderbird"
-hyprctl dispatch exec "xdg-open ~/Nextcloud/Documents/Arbeit/DESY/Arbeitszeiterfassung.ods"
-sleep 2
-hyprctl dispatch workspace 1
-hyprctl dispatch exec /usr/bin/intellij-idea-ultimate-edition
+hyprctl dispatch closewindow "title:KDE Connect"
+switch-workspace 10
+nmcli connection up "DESY VPN" & sleep 0.5 && hyprctl dispatch focuswindow "initialtitle:privat.kdbx \[Locked\] - KeePassXC"
+
+switch-workspace 2
+switch-workspace 1
+switch-workspace 10
+
+# switch-workspace 1
