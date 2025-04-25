d086e6db06e628a0223e3df27f6bcc3f1695827e
Lars Janssen <lars.e.janssen@protonmail.com>
2025-04-25 13:48:21 +0200

    scripts

--- scripts/startcheck.sh
+++ scripts/startcheck.sh
@@ -2,5 +2,5 @@ checkgitrepo () {
     /home/l/.local/cron/git_repo_check_cron.sh "$1" 10000 no-wait
 }
 
-checkgitrepo /home/l/dotfiles/
-checkgitrepo /home/l/Projects/ansible/
+checkgitrepo "/home/l/dotfiles/"
+checkgitrepo "/home/l/Projects/private/ansible/"
