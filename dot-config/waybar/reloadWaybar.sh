153e40b84fa796cab9ea3bcba43fe4dadf609e3d
Lars Janssen <lars.e.janssen@protonmail.com>
2025-06-23 13:07:29 +0200

    waybar

--- dot-config/waybar/reloadWaybar.sh
+++ dot-config/waybar/reloadWaybar.sh
@@ -1,4 +1,31 @@
 #! /bin/bash
 killall waybar
 sleep 0.2
-waybar --config ~/.config/waybar/config.jsonc &
+
+final_config="/tmp/assembled_waybar_config.jsonc"
+# final_config="/home/l/.config/waybar/test.jsonc"
+
+lines_config="$(cat ~/.config/waybar/config.jsonc | wc -l)"
+config="$(cat config.jsonc | head --lines=$(($lines_config-1)) | tail --lines=$(($lines_config-3)))"
+
+if [[ "$HOST" == "archlaptop" ]]; then
+    chosen_layout="/home/l/.config/waybar/configLaptop.jsonc"
+elif [[ "$HOST" == "archPC" ]]; then
+    chosen_layout="/home/l/.config/waybar/configPC.jsonc"
+else
+    chosen_layout="/home/l/.config/waybar/configLaptop.jsonc"
+fi
+
+lines_layout="$(cat $chosen_layout | wc -l)"
+layout="$(cat $chosen_layout | head --lines=$(($lines_layout-1)) | tail --lines=$(($lines_layout-3)))"
+
+rm $final_config
+touch $final_config
+echo "// -*- mode: json -*-" > $final_config
+echo "{" >> $final_config
+echo "$layout," >> $final_config
+echo "" >> $final_config
+echo "$config" >> $final_config
+echo "}" >> $final_config
+
+waybar --config $final_config &
