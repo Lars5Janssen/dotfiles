09eb38897041c417a20955b0b5343f53a5c54224
Lars Janssen <lars.e.janssen@protonmail.com>
2025-06-28 10:54:00 +0200

    formatting

--- dot-config/nvim/lua/plugins/harpoon.lua
+++ dot-config/nvim/lua/plugins/harpoon.lua
@@ -1,66 +1,66 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "folke/which-key.nvim",
-    },
-    config = function()
-        local harpoon = require("harpoon")
-        local wk = require("which-key")
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"folke/which-key.nvim",
+	},
+	config = function()
+		local harpoon = require("harpoon")
+		local wk = require("which-key")
 
-        harpoon:setup()
-        wk.add({
-            { "<leader>h", group = "harpoon" },
-            {
-                "<leader>ha",
-                function()
-                    harpoon:list():add()
-                end,
-                desc = "Add File to Harpoon",
-            },
-            {
-                "<leader>he",
-                function()
-                    harpoon.ui:toggle_quick_menu(harpoon:list())
-                end,
-                desc = "Toggle Harpoon Menu",
-            },
-            {
-                "<leader>h1",
-                function()
-                    harpoon:list():select(1)
-                end,
-                desc = "Select first harpoon entry",
-            },
-            {
-                "<leader>h2",
-                function()
-                    harpoon:list():select(2)
-                end,
-                desc = "Select second harpoon entry",
-            },
-            {
-                "<leader>h3",
-                function()
-                    harpoon:list():select(3)
-                end,
-                desc = "Select third harpoon entry",
-            },
-            {
-                "<leader>h4",
-                function()
-                    harpoon:list():select(4)
-                end,
-                desc = "Select fourth harpoon entry",
-            },
-        })
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-P>", function()
-            harpoon:list():prev()
-        end)
-        vim.keymap.set("n", "<C-N>", function()
-            harpoon:list():next()
-        end)
-    end,
+		harpoon:setup()
+		wk.add({
+			{ "<leader>h", group = "harpoon" },
+			{
+				"<leader>ha",
+				function()
+					harpoon:list():add()
+				end,
+				desc = "Add File to Harpoon",
+			},
+			{
+				"<leader>he",
+				function()
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle Harpoon Menu",
+			},
+			{
+				"<leader>h1",
+				function()
+					harpoon:list():select(1)
+				end,
+				desc = "Select first harpoon entry",
+			},
+			{
+				"<leader>h2",
+				function()
+					harpoon:list():select(2)
+				end,
+				desc = "Select second harpoon entry",
+			},
+			{
+				"<leader>h3",
+				function()
+					harpoon:list():select(3)
+				end,
+				desc = "Select third harpoon entry",
+			},
+			{
+				"<leader>h4",
+				function()
+					harpoon:list():select(4)
+				end,
+				desc = "Select fourth harpoon entry",
+			},
+		})
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-P>", function()
+			harpoon:list():prev()
+		end)
+		vim.keymap.set("n", "<C-N>", function()
+			harpoon:list():next()
+		end)
+	end,
 }
--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -1,82 +1,84 @@
 return {
-        'neovim/nvim-lspconfig',
-        dependencies = {
-            "folke/which-key.nvim",
-            "williamboman/mason.nvim",
-            'williamboman/mason-lspconfig.nvim',
-            'hrsh7th/nvim-cmp',
-            'hrsh7th/cmp-nvim-lsp',
-            'L3MON4D3/LuaSnip',
-        },
-        config = function()
-            local ensure_installed = {
-                    'vtsls',
-                    'html',
-                    "lua_ls",
-                    "gopls",
-                }
-            local cmp = require('cmp')
-            local luasnip = require('luasnip')
-            cmp.setup({
-                sources = {
-                { name = 'nvim_lsp' },
-                },
-                	mapping = {
-		["<C-p>"] = cmp.mapping.select_prev_item(),
-		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.close(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Insert,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif require("luasnip").expand_or_jumpable() then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif require("luasnip").jumpable(-1) then
-				vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-			else
-				fallback()
-			end
-		end, {
-			"i",
-			"s",
-		}),
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"folke/which-key.nvim",
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
 	},
+	config = function()
+		local ensure_installed = {
+			"vtsls",
+			"html",
+			"lua_ls",
+			"gopls",
+		}
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		cmp.setup({
+			sources = {
+				{ name = "nvim_lsp" },
+			},
+			mapping = {
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						vim.fn.feedkeys(
+							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+							""
+						)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+			},
 
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end
-                },
-            })
-            local lspconfig = require('lspconfig')
-            local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-            require("mason").setup({})
-            require('mason-lspconfig').setup({
-                ensure_installed = ensure_installed,
-                handlers = {
-                    function(server)
-                        lspconfig[server].setup({
-                            capabilities = lsp_capabilities,
-                            })
-                    end,
-                },
-            })
-
-        end,
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+		})
+		local lspconfig = require("lspconfig")
+		local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = ensure_installed,
+			handlers = {
+				function(server)
+					lspconfig[server].setup({
+						capabilities = lsp_capabilities,
+					})
+				end,
+			},
+		})
+	end,
 }
